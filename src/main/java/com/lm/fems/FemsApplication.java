8b1316f9c4633fdf99cfe3a852a76a39ecccf9ef
louiemain <louiema1n@github.com>
2017-11-28 17:14:02 +0800

    Initial commit

---
+++ src/main/java/com/lm/fems/FemsApplication.java
@@ -0,0 +1,12 @@
+package com.lm.fems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FemsApplication.class, args);
+	}
+}
